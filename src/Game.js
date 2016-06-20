29dc0e86f9a5532e8ac327ae7cd0c941e1cf2e03
Liang Yue Joseph Chin <CHIN@Liangs-MacBook-Pro.local>
2016-06-20 17:00:00 +0100

    changed functionality

--- src/Game.js
+++ src/Game.js
@@ -39,148 +39,282 @@ function shuffle(array) {
 
   return array;
 }
-function Game(players) {
-  this._playerNumber = players;
-  this._time = "night";
-  this._ingameWerewolfCount = null;
-  this._ingameVillagerCount = null;
-  this._wolfCount = null;
-  this._villagerCount = null;
-  this._players = [];
-  this._playerNames = [];
-  this._deadPlayers = [];
-  this._roleArray = [];
-  this.NIGHT_TIME = "It's night time!";
-}
+// var Game {
+//   _time: "night",
+//   _mode: null,
+//   players: [],
+//   _deadPlayers: [],
+//   _roleArray: []
+// }
+// // not necessarily going to keep Game
+// Game.addPlayers = function(playerArray) {
+//   Game.players = playerArray;
+// }
+//
+// Game.addMode = function(modeIndex) {
+//   Game._mode = wwRules.gameModes[modeIndex];
+// }
+//
+// Game.startWithMode = function(modeNumber) {
+//   Game.mode = wwRules.gameModes[modeNumber]
+// }
+//
+// Game.listMatchedPlayersByType = function(matchfn) {
+//     var playerList = [];
+//     for (var i = 0; i < Game._playerNumber; i++) {
+//         if (matchfn(Game.players[i])) {
+//             playerList.push(Game.players[i]);
+//         }
+//     }
+//     return playerList;
+// };
+//
+// Game.getLivingPlayersList = function() {
+//     return Game.listMatchedPlayersByType(function(p) { return p.isAlive(); });
+// };
+//
+// Game.getLivingWolvesList = function() {
+//     return Game.listMatchedPlayersByType(function(p) { return p.isAlive() && p._role == ww.Roles.Werewolf; });
+// };
+//
+// Game.getLivingNonWolvesList = function() {
+//     return Game.listMatchedPlayersByType(function(p) { return p.isAlive() && p._role != ww.Roles.Werewolf; });
+// };
+//
+// Game.getFullEvilTeam = function() {
+//     return Game.listMatchedPlayersByType(function(p) { return p._role.team == "Werewolves"; });
+// };
+//
+// Game.getFullGoodTeam = function() {
+//     return Game.listMatchedPlayersByType(function(p) { return p._role.team == "Village"; });
+// };
+//
+// Game._addPlayers = function (array) {
+//   this.players = array;
+// };
+// Game._roleAssignment = function () {
+//   var wolfCount = Game.mode.roles[0].count;
+//   for (var i = 0; i < wolfCount; i++){
+//     this._roleArray.push("werewolf");
+//   }
+//   var villagerCount = this.mode.roles[1].count;
+//   for (var i = 0; i < villagerCount; i++){
+//     this._roleArray.push("villager");
+//   }
+//   var roles = this._roleArray;
+//   var shuffledRoles = shuffle(roles);
+//
+//   var assignedRolesArray = this.players.map(function(e, i) {
+//     return [e, shuffledRoles[i]];
+//   });
+//   this._assign(assignedRolesArray);
+// };
+//
+// Game._assign = function (roleArray) {
+//   var arrayLength = roleArray.length;
+//   for (var i = 0; i < arrayLength; i++) {
+//     if(this.roleArray[i][1] === "werewolf") {
+//       this.roleArray[i][0].assign(wwRules.roles.Werewolf);
+//     } else if(this.roleArray[i][1] === "villager") {
+//       this.roleArray[i][0].assign(wwRules.roles.Villager);
+//     }
+//   }
+// };
+// Game._updateTime = function () {
+//   Game.curTime === "day" ? this._time ="night" : this._time = "day"
+// };
+// // Game._headCount = function () {
+// //   this._resetCount();
+// //   this._countPlayers();
+// // };
+//
+// // Game._countPlayers = function () {
+// //   var playerCount = this.players.length;
+// //   var players = this.players;
+// //   var game = this;
+// //   for (var i = 0; i < playerCount; i++) {
+// //     if((players[i].isAlive())) {
+// //       if(players[i]._role === "werewolf") {
+// //         game._ingameWerewolfCount++;
+// //       }
+// //       else {
+// //         game._ingameVillagerCount++;
+// //       }
+// //     }
+// //   }
+// // };
+// //
+// // Game._resetCount = function () {
+// //   this._ingameWerewolfCount = 0;
+// //   this._ingameVillagerCount = 0;
+// // };
+//
+// Game._assignRoles = function () {
+//   // var playerCount = this.players.length;
+//
+//   // this._howManyWolves(this.playerCount());
+//   // this._howManyVillagers();
+//   this._roleAssignment();
+// };
+//
+// // Game.playerCount = function () {
+// //   return  this.players.length;
+// // };
+//
+//
+//
+// Game.isOver = function () {
+//  return this.getFullEvilTeam().length === this.getGullGoodTeam().length || this.getGullEvilTeam().length === 0;
+// };
+// Game._isDay = function () {
+//   return this._time === "day";
+// };
+// Game._outputTime = function () {
+//   return this.NIGHT_TIME;
+// };
+// Game._dayVote = function() {
+//   var voteCount = {};
+//   var playerCount = this.players.length;
+//   var players = this.players;
+//   for(var i = 0; i < playerCount; i++) {
+//     countVotes(player[i]);
+//   }
+//   var sortArray = getSortedKeys(voteCount);
+//   var deadPlayer = sortArray[0];
+//   var toDie = findNamedPlayer(deadPlayer, players);
+//   toDie._kill();
+//   this._deadPlayers.push(toDie);
+//   var index = players.indexOf(toDie);
+//   if (index > -1) {
+//     players.splice(index, 1);
+//   }
+//   this._updateTime();
+// }
+//
+// Game.dayVote = function() {
+//
+// }
+//
+// Game._nightVote = function () {
+//   var voteCount = {};
+//   var playerCount = this.players.length;
+//   var players = this.players
+//   for(var i = 0; i < playerCount; i++) {
+//     if(players[i]._role === "werewolf"){
+//       countVotes(player[i]);
+//     }
+//   };
+//   var sortArray = getSortedKeys(voteCount)
+//
+//   var deadPlayer = sortArray[0]
+//   var players = this.players;
+//   var deadPlayers = this._deadPlayers;
+//   var toDie = findNamedPlayer(deadPlayer, players);
+//
+//   toDie._kill()
+//   deadPlayers.push(toDie);
+//   var index = players.indexOf(toDie);
+//   if (index > -1) {
+//     players.splice(index, 1);
+// }
+// debugger
+//
+//   this._updateTime();
+//
+// };
 
-Game.prototype._addPlayers = function (array) {
-  this._players = array;
-};
-Game.prototype._howManyWolves = function (numberOfPlayers) {
-  this._wolfCount = Math.floor(numberOfPlayers/4);
-};
-Game.prototype._howManyVillagers = function () {
-  this._villagerCount = this._playerNumber - this._wolfCount;
-};
-Game.prototype._roleAssignment = function () {
-  var wolfCount = this._wolfCount;
-  for (var i = 0; i < wolfCount; i++){
-    this._roleArray.push("werewolf");
-  }
-  var villagerCount = this._villagerCount;
-  for (var i = 0; i < villagerCount; i++){
-    this._roleArray.push("villager");
-  }
-  var roles = this._roleArray;
-  var shuffledRoles = shuffle(roles);
+var Game = {};
 
-  var assignedRolesArray = this._players.map(function(e, i) {
-    return [e, shuffledRoles[i]];
-  });
-  this._assign(assignedRolesArray);
-};
+(function() {
 
-Game.prototype._assign = function (roleArray) {
-  var arrayLength = roleArray.length;
-  for (var i = 0; i < arrayLength; i++) {
-    roleArray[i][0].assign(roleArray[i][1]);
-  }
-};
-Game.prototype._updateTime = function () {
-  this._time === "day" ? this._time ="night" : this._time = "day"
-};
-Game.prototype._headCount = function () {
-  this._resetCount();
-  this._countPlayers();
-};
+    Game.numPlayers = 0;
 
-Game.prototype._countPlayers = function () {
-  var playerCount = this._players.length;
-  var players = this._players;
-  var game = this;
-  for (var i = 0; i < playerCount; i++) {
-    if((players[i].isAlive())) {
-      if(players[i]._role === "werewolf") {
-        game._ingameWerewolfCount++;
-      }
-      else {
-        game._ingameVillagerCount++;
-      }
+    Game.curTurnIndex = 0;
+    Game.curTime = "night";
+    Game.mode = null;
+    Game._roleArray = []
+    Game.players = [];
+
+    Game.nkPlayerIndex = -1;
+
+    Game.maxNightActions = 0;
+    Game.wolfSuggestList = [];
+
+    Game.addMode = function(modeIndex) {
+      Game.mode = wwRules.gameModes[modeIndex];
     }
-  }
-};
 
-Game.prototype._resetCount = function () {
-  this._ingameWerewolfCount = 0;
-  this._ingameVillagerCount = 0;
-};
+    Game.listMatchedPlayersByType = function(matchfn) {
+        var playerList = [];
+        for (var i = 0; i < Game.numPlayers; i++) {
+            if (matchfn(Game.players[i])) {
+                playerList.push(Game.players[i]);
+            }
+        }
+        return playerList;
+    };
 
-Game.prototype._assignRoles = function () {
-  // var playerCount = this._players.length;
+    Game.getLivingPlayersList = function() {
+        return Game.listMatchedPlayersByType(function(p) { return p.isAlive(); });
+    };
 
-  this._howManyWolves(this.playerCount());
-  this._howManyVillagers();
-  this._roleAssignment();
-};
+    Game.getLivingWolvesList = function() {
+        return Game.listMatchedPlayersByType(function(p) { return p.isAlive() && p._role == wwRules.roles.Werewolf; });
+    };
 
-Game.prototype.playerCount = function () {
-  return  this._players.length;
-};
+    Game.getLivingNonWolvesList = function() {
+        return Game.listMatchedPlayersByType(function(p) { return p.isAlive() && p._role != wwRules.roles.Werewolf; });
+    };
 
+    Game.getFullEvilTeam = function() {
+        return Game.listMatchedPlayersByType(function(p) { return p._role.team == "Werewolves"; });
+    };
 
+    Game.getFullGoodTeam = function() {
+        return Game.listMatchedPlayersByType(function(p) { return p._role.team == "Village"; });
+    };
 
-Game.prototype.isOver = function () {
- return this._ingameWerewolfCount === this._ingameVillagerCount || this._ingameWerewolfCount === 0;
-};
-Game.prototype._isDay = function () {
-  return this._time === "day";
-};
-Game.prototype._outputTime = function () {
-  return this.NIGHT_TIME;
-};
-Game.prototype._dayVote = function() {
-  var voteCount = {};
-  var playerCount = this._players.length;
-  var players = this._players;
-  for(var i = 0; i < playerCount; i++) {
-    countVotes(player[i]);
-  }
-  var sortArray = getSortedKeys(voteCount);
-  var deadPlayer = sortArray[0];
-  var toDie = findNamedPlayer(deadPlayer, players);
-  toDie._kill();
-  this._deadPlayers.push(toDie);
-  var index = players.indexOf(toDie);
-  if (index > -1) {
-    players.splice(index, 1);
-  }
-  this._updateTime();
-}
-
-Game.prototype._nightVote = function () {
-  var voteCount = {};
-  var playerCount = this._players.length;
-  var players = this._players
-  for(var i = 0; i < playerCount; i++) {
-    if(players[i]._role === "werewolf"){
-      countVotes(player[i]);
+    Game.clearRoleArray = function() {
+      Game._roleArray = [];
     }
-  };
-  var sortArray = getSortedKeys(voteCount)
 
-  var deadPlayer = sortArray[0]
-  var players = this._players;
-  var deadPlayers = this._deadPlayers;
-  var toDie = findNamedPlayer(deadPlayer, players);
+    Game._addPlayers = function (array) {
+      Game.players = array;
+      Game.numPlayers = Game.players.length;
+    };
+    Game._roleAssignment = function () {
+      var wolfCount = Game.mode.roles[0].count;
+      for (var i = 0; i < wolfCount; i++){
+        this._roleArray.push("werewolf");
+      }
+      var villagerCount = this.mode.roles[1].count;
+      for (var i = 0; i < villagerCount; i++){
+        this._roleArray.push("villager");
+      }
+      var roles = this._roleArray;
+      var shuffledRoles = shuffle(roles);
 
-  toDie._kill()
-  deadPlayers.push(toDie);
-  var index = players.indexOf(toDie);
-  if (index > -1) {
-    players.splice(index, 1);
-}
-debugger
+      var assignedRolesArray = this.players.map(function(e, i) {
+        return [e, shuffledRoles[i]];
+      });
+      this._assign(assignedRolesArray);
+    };
 
-  this._updateTime();
+    Game._assign = function (roleArray) {
+      var arrayLength = roleArray.length;
+      for (var i = 0; i < arrayLength; i++) {
+        if(roleArray[i][1] === "werewolf") {
+          roleArray[i][0].assign(wwRules.roles.Werewolf);
+        } else if(roleArray[i][1] === "villager") {
+          roleArray[i][0].assign(wwRules.roles.Villager);
+        }
+      }
+    };
+    Game._updateTime = function () {
+      Game.curTime === "day" ? Game.curTime ="night" : Game.curTime = "day"
+    };
 
-};
+    Game.isOver = function () {
+     return Game.getLivingWolvesList().length === Game.getLivingNonWolvesList().length || Game.getLivingWolvesList().length === 0;
+    };
+})();
